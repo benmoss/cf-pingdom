6a7cbb1463eb2e2551bcc25a527c6880c04fbae4
Amin Jamali <a.jamali@pivotal.io>
2015-08-11 15:16:58 -0400

    Adding notification for restarting the apps
    Signed-off-by: Ben_Moss <bmoss@pivotal.io>

--- cf-pingdom.js
+++ cf-pingdom.js
@@ -17,10 +17,34 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   var CurrentApps = [];
-  var StoppedApps = [];
   var CFClient = Meteor.npmRequire('cloudfoundry-client');
   var WebhookURL = 'https://hooks.slack.com/services/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'
   var SlackClient = Meteor.npmRequire('slack-notify')(WebhookURL);
+
+  function transformResult(input){
+    return _.map(input, function(i) {
+      return {
+        _id: i.metadata.guid,
+        entity: i.entity
+      }})
+  }
+
+  function compare(apps, apiResponse) {
+    parsedApiResponse = transformResult(apiResponse);
+    var diff = [];
+    _.forEach(parsedApiResponse,function(i){
+      if (!_.find(apps,function(app){
+        return app._id === i._id && app.entity.state === i.entity.state;
+      })) {
+        diff.push(i);
+      }
+    });
+
+    return _.filter(parsedApiResponse,function(i){
+      return _.find(diff,{_id:i._id});
+    });
+
+  }
 
   Meteor.startup(function () {
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
@@ -37,25 +61,22 @@ if (Meteor.isServer) {
           done(err, apps);
         });
       });
-
-      CurrentApps = response.result;
-      _.forEach(response.result,function(item){
-        Apps.update({_id:item.metadata.guid},{
-          _id: item.metadata.guid,
-          entity: item.entity
+      CurrentApps = Apps.find().fetch();
+      var different = compare(CurrentApps, response.result)
+      _.forEach(different,function(app){
+        Apps.update({_id:app._id},{
+          _id: app._id,
+          entity: app.entity
         },{upsert:true});
-      });
-      var stopped = _.filter(CurrentApps,function(i){
-        return i.entity.state == 'STOPPED' && !_.find(StoppedApps,function(sp){
-          return sp.entity.name == i.entity.name;
-        });
-      });
-      StoppedApps = _.union(StoppedApps,stopped);
-      _.forEach(stopped,function(i){
-        SlackClient.send({
-          channel: '#buildpack-webhook',
-          text: i.entity.name + ' has stopped running!'
-        });
+
+        //notify
+        if (app.entity.state === "STOPPED") {
+          // console.log("slack", app.entity.name)
+          SlackClient.send({
+            channel: '#buildpack-webhook',
+            text: app.entity.name + ' has stopped running!'
+          });
+        }
       });
     },1000);
   });
@@ -63,8 +84,6 @@ if (Meteor.isServer) {
     'getApps': function() {
       return CurrentApps;
     },
-    'notify': function(message){
-    }
   })
 }
 
